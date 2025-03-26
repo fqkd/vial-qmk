b750de1b9da9438500afb11f051f785ade4d95c5
Danil Tolkachev <dandur.oak@gmail.com>
2025-03-26 16:33:45 +0300

    add mirror modules for hpd2

--- keyboards/ergohaven/hpd/keymaps/v2_enc_enc/keymap.c
+++ keyboards/ergohaven/hpd/keymaps/v2_enc_enc/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "ergohaven.h"
+#include "ergohaven_pointing.h"
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -40,3 +41,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 // clang-format on
+
+#ifdef ENCODER_MAP_ENABLE
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [1] = {ENCODER_CCW_CW(_______, _______),ENCODER_CCW_CW(_______, _______)},
+    [2] = {ENCODER_CCW_CW(_______, _______),ENCODER_CCW_CW(_______, _______)},
+    [3] = {ENCODER_CCW_CW(_______, _______),ENCODER_CCW_CW(_______, _______)},
+};
+#endif
---
+++ keyboards/ergohaven/hpd/keymaps/v2_enc_enc/rules.mk
@@ -0,0 +1,9 @@
+VIAL_ENABLE = yes
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+RGBLIGHT_ENABLE = yes
+RGBLIGHT_DRIVER = ws2812
+WS2812_DRIVER = vendor
+
+SRC += keyboards/ergohaven/ergohaven_rgb.c
