c433568326a0d035d8691badf56390c88d4ec46d
Danil Tolkachev <dandur.oak@gmail.com>
2025-07-15 18:38:36 +0300

    remove thumbs from chordal hold

--- keyboards/ergohaven/planeta/rev2/rev2.c
+++ keyboards/ergohaven/planeta/rev2/rev2.c
@@ -8,7 +8,7 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
 'L', 'L', 'L', 'L', 'L', 'L',                       'R', 'R', 'R', 'R', 'R', 'R',
 'L', 'L', 'L', 'L', 'L', 'L',                       'R', 'R', 'R', 'R', 'R', 'R',
 'L', 'L', 'L', 'L', 'L', 'L',                       'R', 'R', 'R', 'R', 'R', 'R',
-          'L', 'L', 'L', 'L', 'L', 'L',   'R', 'R', 'R', 'R', 'R', 'R'
+          '*', '*', '*', '*', '*', '*',   '*', '*', '*', '*', '*', '*'
 );
 
 void housekeeping_task_user(void) {
