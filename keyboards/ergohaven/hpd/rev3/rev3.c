42daf90bb865822e9c843882fe1ce949b13f4ca4
Danil Tolkachev <dandur.oak@gmail.com>
2026-03-15 10:59:55 +0300

    copy rev2 to rev3

---
+++ keyboards/ergohaven/hpd/rev3/rev3.c
@@ -0,0 +1,14 @@
+// Copyright 2022 Ergohaven (@ergohaven)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#include QMK_KEYBOARD_H
+#include "ergohaven.h"
+
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
+'L', 'L', 'L', 'L', 'L', 'L',                       'R', 'R', 'R', 'R', 'R', 'R',
+'L', 'L', 'L', 'L', 'L', 'L',                       'R', 'R', 'R', 'R', 'R', 'R',
+'L', 'L', 'L', 'L', 'L', 'L',                       'R', 'R', 'R', 'R', 'R', 'R',
+'L', 'L', 'L', 'L', 'L', 'L',                       'R', 'R', 'R', 'R', 'R', 'R',
+          'L', 'L', '*', '*', '*', '*',   '*', '*', '*', '*', 'R', 'R',
+                                   '*',   '*'
+
+);
